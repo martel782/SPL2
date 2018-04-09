4760be67df75ff2c79b5a50d173efaa71f8ad1c0
martel782 <171wita10@o365.htl-leoben.at>
2018-04-09 14:49:56 +0200

    Erweiterung weiterführen und Fehlerpages

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -6,15 +6,15 @@ passagiere = 0
 for i in range(zahl1):
     print("Der Bus ist an haltestelle ",i, " angekommen")
     passagiereminus = int(input("Bitte geben Sie die zahl an Personen an die Austeigen"))
-    passagiereplus = int(input("Bitte geben Sie an wie viele Passagiere einsteigen"))
-    if (passagiereplus+passagiere)<60:
-
-        passagiere = passagiere + passagiereplus
-        passagiere1 = passagiere - passagiereminus
+    if passagiere1 < 60:
+        passagiereplus = int(input("Bitte geben Sie an wie viele Passagiere einsteigen"))
+    if passagiere1 >= 60:
+            print("Es dürfen nur mehr Passagiere austeigen")
+    passagiere = passagiere + passagiereplus
+    passagiere1 = passagiere - passagiereminus
+    if passagiere1<60:
         print("Es sind ",passagiere1," im Bus")
-        if passagiere1 > 60:
-            print("Es sind zu viele Passagiere im Bus")
-            ab = passagiere1 - 60
-            print("Es müssen",ab," Personen austeigen")
-    else: print("Achtung Sie überschreiten die Max Anzahl an Personen im Bus")
-    
+    else:
+        print("Es sind zu viele Passagiere im Bus")
+        ab = passagiere1 - 60
+        print("Es müssen",ab," Personen austeigen")
