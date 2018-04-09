03c6cd7e3e42fd7ea1132eb6c522225e11650c2e
martel782 <171wita10@o365.htl-leoben.at>
2018-04-09 14:17:38 +0200

    Erstellung der V1 des Programmes

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -1,1 +1,11 @@
 #bus-und-haltestelle
+live = "Herzlich Willkommen zum Bus simulator"
+print(live);
+zahl1 = int( input("Bitte geben Sie ein wie viele Haltestellen es gibt"))
+passagiere = 0
+for i in range(zahl1):
+    print("Der Bus ist an haltestelle ",i, " angekommen")
+    passagiereplus = int(input("Bitte geben Sie an wie viele Passagiere einsteigen"))
+    passagiere = passagiere + passagiereplus
+    print("Es sind ",passagiere," im Bus")
+    
