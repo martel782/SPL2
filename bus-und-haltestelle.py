3c2bf6b78de4cd644b5956d8b50c23c5f72742bc
martel782 <171wita10@o365.htl-leoben.at>
2018-04-09 14:34:44 +0200

    Erweiterungen installation

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -5,7 +5,16 @@ zahl1 = int( input("Bitte geben Sie ein wie viele Haltestellen es gibt"))
 passagiere = 0
 for i in range(zahl1):
     print("Der Bus ist an haltestelle ",i, " angekommen")
+    passagiereminus = int(input("Bitte geben Sie die zahl an Personen an die Austeigen"))
     passagiereplus = int(input("Bitte geben Sie an wie viele Passagiere einsteigen"))
-    passagiere = passagiere + passagiereplus
-    print("Es sind ",passagiere," im Bus")
+    if (passagiereplus+passagiere)<60:
+
+        passagiere = passagiere + passagiereplus
+        passagiere1 = passagiere - passagiereminus
+        print("Es sind ",passagiere1," im Bus")
+        if passagiere1 > 60:
+            print("Es sind zu viele Passagiere im Bus")
+            ab = passagiere1 - 60
+            print("Es müssen",ab," Personen austeigen")
+    else: print("Achtung Sie überschreiten die Max Anzahl an Personen im Bus")
     
